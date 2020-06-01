37644fe1df3f16ad1232ac36acc96eb4e62969e0
unknown <cindy.mendoza@harrison.com.pe>
2020-06-01 17:34:25 -0500

    config web pack

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,38 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+  .sass('resources/sass/app.scss', 'public/css')
+  .sourceMaps();
+mix.browserSync('http://127.0.0.1:8000');
+
+//Third party packages
+const ImageminPlugin     = require('imagemin-webpack-plugin').default;
+const CopyWebpackPlugin  = require('copy-webpack-plugin');
+const imageminMozjpeg    = require('imagemin-mozjpeg');
+
+//Only in production, you can remove if you want to use "npm run dev"
+// if(mix.inProduction()) {
+    mix.webpackConfig({
+        plugins: [
+            //Compress images
+            new CopyWebpackPlugin([{
+                from: 'resources/img', // FROM
+                to: 'images/', // TO
+            }]),
+            new ImageminPlugin({
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                pngquant: {
+                    quality: '65-80'
+                },
+                plugins: [
+                    imageminMozjpeg({
+                        quality: 65,
+                        //Set the maximum memory to use in kbytes
+                        maxmemory: 1000 * 512
+                    })
+                ]
+            })
+        ],
+    });
+// }
+
